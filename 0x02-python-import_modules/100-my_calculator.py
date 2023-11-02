608ec7a7efd794a53276df36e392cbc5799a611f
Cheikyadam <urbaingande@gmail.com>
2023-11-02 14:17:42 -0700

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,14 +3,13 @@ if __name__ == "__main__":
     import calculator_1 as c
     import sys as s
     if len(s.argv) != 4:
-        s.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(s.argv[1])
     b = int(s.argv[3])
     op = s.argv[2]
     if op not in "+-*/":
-        s.stderr.write("Unknown operator. Available ")
-        s.stderr.write("operators: +, -, * and /\n")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     if op == '+':
         print("{:d} + {:d} = {:d}".format(a, b, c.add(a, b)))
