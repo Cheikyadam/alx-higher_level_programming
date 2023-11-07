c94215b1eb2420438676b7f59a461c3dea0fb301
Cheikyadam <urbaingande@gmail.com>
2023-11-06 16:20:31 -0800

    plus

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    even_list = []
+    for num in my_list:
+        if num % 2 == 0:
+            even_list.append(True)
+        else:
+            even_list.append(False)
+    return even_list
