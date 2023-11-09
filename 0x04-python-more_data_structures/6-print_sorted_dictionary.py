485800dd4b468c3bd684d4482716c7ee77b1824a
Cheikyadam <urbaingande@gmail.com>
2023-11-08 16:05:31 -0800

    6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sort = sorted((list(a_dictionary)))
+    for key in sort:
+        print("{:s}: {}".format(key, a_dictionary[key]))
