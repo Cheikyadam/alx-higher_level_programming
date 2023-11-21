c39bfbd9d970914defa013d057b33fcb426b251f
Cheikyadam <urbaingande@gmail.com>
2023-11-21 15:31:31 -0800

    1doc11

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,4 +9,10 @@
 
 class Square:
     def __init__(self, size):
+        """
+        Initialize a new square.
+
+        Parameters:
+        - size (int): the size of the square
+        """
         self.__size = size
