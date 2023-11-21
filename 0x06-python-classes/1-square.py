5be979780460740279ed27d0a565f64b21a52f8a
Cheikyadam <urbaingande@gmail.com>
2023-11-21 14:36:09 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """Simple class to represent a square.
+
+    Args:
+        size: the size of the square
+
+    Attributes:
+        size: the size of the square
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
