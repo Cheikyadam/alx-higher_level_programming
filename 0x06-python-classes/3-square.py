f32c625c7d8050a2a7cca9404fd672b574625a52
Cheikyadam <urbaingande@gmail.com>
2023-11-21 16:59:14 -0800

    03

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+"""
+Simple class to represent a square.
+
+Attributes:
+    __size (int): the size of the square
+"""
+
+
 class Square:
-    """
-    Simple class to represent a square.
-
-    Attributes:
-        __size (int): the size of the square
-    """
-
     def __init__(self, size=0):
         """
         Initializes a new square.
