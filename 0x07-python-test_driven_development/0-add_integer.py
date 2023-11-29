4b97b3fcc88077feb0c388135565bed8a1ad6d48
Cheikyadam <urbaingande@gmail.com>
2023-11-29 10:15:08 -0800

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+    To add two integers
+
+    Parameters:
+        a (int): the firs parameter
+        b (int): the second one
+
+    Returns:
+        int: the sum of a and b
+    """
     if type(a) is not int:
         if type(a) is not float:
             raise TypeError("a must be an integer")
