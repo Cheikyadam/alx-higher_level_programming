efe2456ff3d2c71edf85c441f30fc095e6652b6e
Cheikyadam <urbaingande@gmail.com>
2023-11-29 11:41:10 -0800

    0 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -21,6 +21,9 @@ def add_integer(a, b=98):
     if type(b) is not int:
         if type(b) is not float:
             raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
     a = int(a)
     b = int(b)
     return a+b
