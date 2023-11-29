45a96ea31afbb3d5b56dd40a556fa2f233b04022
Cheikyadam <urbaingande@gmail.com>
2023-11-29 15:12:53 -0800

    4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+"""
+Simple function to print a square with #
+"""
+
+
 def print_square(size):
+    """
+    Definition of the function
+
+    Parameters:
+        size (int): the sizr of the square
+    """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print()
-    else:
+    if size != 0:
         for i in range(0, size):
             for j in range(0, size):
                 print("#", end="")
