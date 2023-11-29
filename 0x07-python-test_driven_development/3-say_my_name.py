0b6887af8decc837a7a2c380718f961d0ba6cfad
Cheikyadam <urbaingande@gmail.com>
2023-11-29 14:52:22 -0800

    3 3 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+Simple function to print name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Simple function to print names
+
+    Parameters:
+        first_name (str): the first parameter
+        last_name (str): the second
+
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
