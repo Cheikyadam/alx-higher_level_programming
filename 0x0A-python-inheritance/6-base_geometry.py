1a1ebb95d81504695e121bd7a432b8163af20e87
Cheikyadam <urbaingande@gmail.com>
2023-12-04 15:51:11 -0800

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class Geometry
+"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+    def area(self):
+        """to find area"""
+        raise Exception("area() is not implemented")
