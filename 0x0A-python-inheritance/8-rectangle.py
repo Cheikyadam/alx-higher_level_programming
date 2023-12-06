a821d5088ae81bacdda127019524859d8db15073
Cheikyadam <urbaingande@gmail.com>
2023-12-06 15:45:46 -0800

    8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,31 +1,8 @@
 #!/usr/bin/python3
 """
-Class Geometry
-"""
-
-
-class BaseGeometry:
-    """Geometry class"""
-    def area(self):
-        """to find area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ to check if value is an integer
-
-        Parameters:
-            name (str): the name of the value
-            value (int): a value given
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-"""
 class Rectangle inherited geometry
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
