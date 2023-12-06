f8cec230cf5b7c33c5186a6d26e74063d917b73f
Cheikyadam <urbaingande@gmail.com>
2023-12-06 10:14:12 -0800

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Class to represents a student
+"""
+
+
+class Student:
+    """The class
+
+    Attributes:
+        first_name (str)
+        last_name (str)
+        age (int)
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ To initiate a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Function that returns the json represnetation \
+                of a student
+        """
+        if attrs is None:
+            return self.__dict__
+        dico = dict()
+        if "first_name" in attrs:
+            dico['first_name'] = self.first_name
+        if "last_name" in attrs:
+            dico['last_name'] = self.last_name
+        if "age" in attrs:
+            dico['age'] = self.age
+        return dico
