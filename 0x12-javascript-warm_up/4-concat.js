eb305448551c8c7f4b3a011dc309c37c6f0d82d3
Cheikyadam <urbaingande@gmail.com>
2024-02-12 15:25:22 -0800

    4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let first = 'undefined ';
+const second = 'is ';
+let third = 'undefined';
+if (process.argv.length >= 3) {
+  first = process.argv[2] + ' ';
+}
+if (process.argv.length >= 4) {
+  third = process.argv[3] + ' ';
+}
+console.log(first + second + third);
