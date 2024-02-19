50c713a0b8aadc94d3100259a6a4bb6d610028eb
Cheikyadam <urbaingande@gmail.com>
2024-02-19 04:51:45 -0800

    casse

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
             db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+            """SELECT * FROM states WHERE name LIKE 'N%'
+            COLLATE utf8_bin ORDER BY states.id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
