db85e84d6cf7a0672afffcc504025d656ed7dfb4
Cheikyadam <urbaingande@gmail.com>
2024-02-29 18:52:29 -0800

    s opt

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #post
-curl -X GET -H "X-School-User-Id: 98" "$1" 
+curl -sX GET -H "X-School-User-Id: 98" "$1" 
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Posting
-email="test@gmail.com" subject="I will always be here for PLD" && curl -X POST -d "email=$email&subject=$subject" "$1"
+email="test@gmail.com" subject="I will always be here for PLD" && curl -sX POST -d "email=$email&subject=$subject" "$1"
