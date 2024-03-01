533de62cecca88fa3915095cacd1c677b049c84c
Cheikyadam <urbaingande@gmail.com>
2024-02-29 19:01:05 -0800

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,12 @@
-#!/bin/
+#!/usr/bin/python3
+"""Finding a peak"""
+
+
+def find_peak(list_of_integers):
+    """the function"""
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+    # peak = list_of_integers[0]
+    # leng = len(list_of_integers)
+    # i = 0
+    # while
