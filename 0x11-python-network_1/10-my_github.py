9b1442565eba6d2ee4b41ce89aed59d4896ff191
Cheikyadam <urbaingande@gmail.com>
2024-03-02 02:19:04 -0800

    10 up

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
     token = sys.argv[2]
     username = sys.argv[1]
     url = f"https://api.github.com/users/{username}"
-    headers = {'Authorization': f"Bearer{token}"}
+    headers = {'Authorization': f'Bearer {token}'}
     try:
         r = requests.get(url, headers=headers)
         res = r.json()
         print(res['id'])
     except Exception as e:
-        pass
+        print(None)
